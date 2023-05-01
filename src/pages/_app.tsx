d40dbe567d1e36ad441515c4ea270fc568e4bab8
BrenoBTrindade <brenokaminski@hotmail.com>
2023-05-01 18:49:01 -0300

    feat: styledcomponents for project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import React from 'react'
-import GlobalStyle from '@/styles'
+import GlobalStyle from '../styles/globalstyle'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,21 @@
-import Link from '@/components/Link'
-import { Inter } from 'next/font/google'
-import Title from '@/components/Title'
+import HomeScreen from '../screens/HomeScreen/index'
 
-const inter = Inter({ subsets: ['latin'] })
+export default HomeScreen;
+// import Link from '@/components/Link'
+// import { Inter } from 'next/font/google'
+// import Title from '@/components/Title'
+// import Footer from '../components/Footer'
 
-export default function Home() {
-  return (
-    <div>
-        <Title as='h1'>Alura cases - Home</Title>
-        <Link href='/faq'>
-           Ir para a página do FAQ
-        </Link>
-    </div>
-  )
-}
+// const inter = Inter({ subsets: ['latin'] })
+
+// export default function Home() {
+//   return (
+//     <div>
+//         <Title as='h1'>Alura cases - Home</Title>
+//         <Link href='/faq'>
+//            Ir para a página do FAQ
+//         </Link>
+//         <Footer/>
+//     </div>
+//   )
+//}
